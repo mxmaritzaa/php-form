391339132df14d0160372d96fda90c4cc1dc678b
mxmaritzaa <72465537+mxmaritzaa@users.noreply.github.com>
2020-11-19 11:43:12 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-echo'<p>This is PHP!</p>';
+//echo'<p>This is PHP!</p>';
+
+if(isset($_GET["First Name"])){//show feedback
+ echo $_GET["First name"];
+}else{//show form
+ echo '   
+ <form="">
+ First Name <inout type="text" name="FirstName" />
+ <br / .
+ <input type="submit">
+ 
+</form> 
+ ';
+}
+
+
+
 ?>
-<p>This is HTML!</p>
+
